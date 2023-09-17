886e0ef850d606179d32246f806c8ed6a41042f6
Darshan <shah_d@subway.com>
2023-09-17 16:52:29 +0530

    Revert "added crud"
    This reverts commit 7d7f0599d9aac30fd2b0c99c5b94bc5c03de93e3.

--- index.js
+++ index.js
@@ -1,77 +1,11 @@
 require('dotenv').config()
 const express = require("express");
-const mongoose = require("mongoose");
-
-//models
-const TodoTask = require("./models/todotask");
-
 const app = express();
 
 app.use("/static", express.static("public"));
-app.use(express.urlencoded({ extended: true }));
 
-
-//connection to db
-mongoose.connect(process.env.DB_CONNECT).then(() => {
-        console.log("Connected to db!");
-        app.listen(process.env.PORT, () => console.log("Server Up and running"));
-    })
-  .catch((err) => {
-        console.log("Error connecting to DB");
-    });
-
-// GET METHOD
-app.get("/", async (req, res) => {
-    const tasks = await TodoTask.find({});
-    res.render("todo.ejs", { todoTasks: tasks });
+app.get('/',(req, res) => {
+    res.render("todo.ejs");
 });
 
-//POST METHOD
-app.post('/',async (req, res) => {
-    const todoTask = new TodoTask({
-    title: req.body.title,
-    shortDesc: req.body.title
-    });
-    try {
-        await todoTask.save();
-        res.redirect("/");
-    } catch (err) {
-        res.redirect("/");
-    }
-});
-
-//UPDATE
-app.route("/edit/:id")
-    .get(async (req, res) => {
-        const id = req.params.id;
-        const tasks = await TodoTask.find({});
-        res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-    })
-    .post(async (req, res) => {
-        const id = req.params.id;
-        try {
-            await TodoTask.findByIdAndUpdate(id, 
-                { 
-                    title: req.body.title,
-                    shortDesc: req.body.shortDesc 
-                });
-        }
-        catch(err) {
-            return res.send(500, err);
-        }
-        res.redirect("/");
-        
-        // if (err) return res.send(500, err);
-        // res.redirect("/");
-    })
-
-//DELETE
-app.route("/remove/:id").get(async (req, res) => {
-    const id = req.params.id;
-    try {
-        await TodoTask.findByIdAndRemove(id);
-    } catch (err) {
-        return res.send(500, err);
-    }
-    res.redirect("/");
-});
+app.listen(process.env.PORT, () => console.log("Server Up and running"));
