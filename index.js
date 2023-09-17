d6fd795acbee49ec849e8cf9395a766040c45a4f
Darshan <shah_d@subway.com>
2023-09-17 17:11:24 +0530

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 require('dotenv').config()
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+//connection to db
+try {
+    mongoose.connect(process.env.DB_CONNECT);
+    console.log("Connected to MongoDB!");
+    app.listen(3000, () => console.log("Server Up and running"));
+} catch (error) {
+    console.log("Error connecting MongoDB", error);
+}
 
 app.get('/',(req, res) => {
     res.render("todo.ejs");
 });
 
-app.listen(process.env.PORT, () => console.log("Server Up and running"));
+app.post('/',(req, res) => {
+    console.log(req.body);
+});
