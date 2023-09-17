7cef84ceda363c1e7a0a630e05b63ec1df752104
Darshan <shah_d@subway.com>
2023-09-17 17:28:54 +0530

    create todo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
+const TodoTask = require("./models/todotask");
 
+const app = express();
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
 app.set("view engine", "ejs");
 
 //connection to db
@@ -21,6 +21,16 @@ app.get('/',(req, res) => {
     res.render("todo.ejs");
 });
 
-app.post('/',(req, res) => {
-    console.log(req.body);
+//POST METHOD
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+        title: req.body.title,
+        desc: req.body.desc
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } catch (err) {
+        res.redirect("/");
+    }
 });
